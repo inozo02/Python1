27cc8fde3cd5ae1529d6e0c1880f2461e9a5ca05
inozo02 <inezwidholm@gmail.com>
2025-07-04 10:57:35 +0200

    sum

--- test.py
+++ test.py
@@ -1,1 +1,25 @@
-print("hello")
+#calculator easy
+
+
+def startCalculator():
+    print("Choose calculation: Sum")
+    operation= input()
+    if operation =="sum":
+        
+        total_sum=0
+    
+    while True:
+        user_input= input("Enter a number, or write done")
+        
+        if user_input =="done":
+            break
+        try:
+            user_input=float(user_input)
+            total_sum+= user_input
+            print(f"Number is: {user_input}")
+        except:
+            print("invalid number")    
+    print(f"Total sum is: {total_sum}")      
+    
+      
+startCalculator()
